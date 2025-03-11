ac8d94719dba161894bbe226f0d2de25be134c4f
jagath-sajjan <jagathsajjan097@gmail.com>
2025-03-11 20:36:11 +0530

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -4,7 +4,7 @@ javac AreaTool.java
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful!"
-    echo "Run the tool using: java Main"
+    echo "Run the tool using: java AreaTool"
 else
     echo "Compilation failed!"
 fi
